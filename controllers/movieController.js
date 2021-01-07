8d268d143228736264e222d6708de3320bcd1e8f
kubido <kubido@gmail.com>
2021-01-07 13:21:48 +0700

    ini kerjaan kubido #1

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const getPopularMovie = (req, res) => {
-  let popularMovieApiUrl = "https://api.themoviedb.org/3/movie/popular"
+  let popularMovieApiUrl = "https://api.themoviedb.org/3/movie/tidak_popular"
 
   axios.get(popularMovieApiUrl, {
     headers: {
